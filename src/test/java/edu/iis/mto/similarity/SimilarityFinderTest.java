9497a7a46cd42b57cb9f29a6839abc829814f597
krecikovsky112 <krecikovsky112@gmail.com>
2021-03-23 13:40:38 +0100

    Change names of tests, add test when sequences have equal length, but completely different elements

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -11,15 +11,17 @@ import org.junit.jupiter.api.Test;
 class SimilarityFinderTest {
 
     SimilarityFinder finderTrue;
+    SimilarityFinder finderFalse;
 
     @BeforeEach
     public void createFinders()
     {
         finderTrue = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().withFound(true).build()));
+        finderFalse = new SimilarityFinder(((elem, sequence) -> SearchResult.builder().withFound(false).build()));
     }
 
     @Test
-    public void areSequenceLengthsZero() {
+    public void shouldReturnOneWhenLengthsSequenceAreZero() {
         int[] seq1 = {};
         int[] seq2 = {};
 
@@ -35,11 +37,23 @@ class SimilarityFinderTest {
     }
 
     @Test
-    public void areBothSequencesHaveEqualLengthsAndContainSameElements(){
+    public void shouldReturnOneWhenBothSequencesHaveEqualLengthsAndHaveSameElements(){
         int[] seq1 = {4, 5};
         int[] seq2 = {4, 5};
 
         double result = finderTrue.calculateJackardSimilarity(seq1, seq2);
         assertEquals(1.0d, result);
     }
+
+    @Test
+    public void shouldReturnZeroWhenSequencesHaveEqualLengthsAndHaveCompletelyDifferentElements() {
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {4, 5, 6};
+
+        double result = finderFalse.calculateJackardSimilarity(seq1, seq2);
+
+        assertEquals(0, result);
+    }
+
+
 }
